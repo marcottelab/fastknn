c14944f91868b51cffe0c86a486ad3ca5ce1b015
John Woods <john.woods@marcottelab.org>
2010-05-23 17:30:08 -0500

    Simple fixes to phenomatrix extconf.rb

--- ext/phenomatrix/extconf.rb
+++ ext/phenomatrix/extconf.rb
@@ -1,12 +1,8 @@
 require 'rubygems'
 require 'mkmf-rice'
 
-dir_config("phenomatrix")
-dir_config("boost")
-
 have_library("stdc++")
 have_library("pqxx")
-have_library("boost")
 have_library("boost_filesystem")
 if RUBY_VERSION =~ /1.9/ then
   $CPPFLAGS += " -DRUBY_19"
